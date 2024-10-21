1136e1d85f0e6e76974a56e7bfc125bd3e25a505
luciano-viana <lucianoviana12@gmail.com>
2024-10-21 08:41:12 -0300

    Implementado novas funcionalidades na primeirapagina.xhtml

--- projeto-jsf/src/main/java/br/com/cursojsf/pessoaBean.java
+++ projeto-jsf/src/main/java/br/com/cursojsf/pessoaBean.java
@@ -4,23 +4,39 @@ import javax.faces.bean.ManagedBean;
 
 @ManagedBean
 public class pessoaBean {
-	
+
 	private String nome;
 	private String sobreNome;
-	
+	private String nomeCompleto;
+
+	//Metodo mostrarNome
+	public String mostrarNome() {
+		nomeCompleto = nome + "" + sobreNome;
+		return "";
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getSobreNome() {
 		return sobreNome;
 	}
+
 	public void setSobreNome(String sobreNome) {
 		this.sobreNome = sobreNome;
 	}
-	
-	
 
 }
